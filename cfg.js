6a7b0acb28a46374d08ef267127788e867e9f341
Ayberk DEMİR <77262747+AtaGalata@users.noreply.github.com>
2023-02-01 14:33:01 +0300

    Update cfg.js

--- cfg.js
+++ cfg.js
@@ -4,7 +4,7 @@ MADE BY AtaGalata
 
 module.exports = {
     token : "" || process.env.token, //Glitch e Yüklerseniz Burayı Boş Bırakın, .env Dosyasının İçine Yazın
-    süre : 5 * 1000, //Ne KadaR Sürede Değişceği (min: 5 sn)
+    süre : 10 * 1000, //Ne Kadar Sürede Değişceği (min: 10 sn yoksa buga giriyor)
 
 /*      1 sn = 1000
         5 sn = 5 * 1000
